e8de1785c1634dea698441eed6a4d5a91216812b
atgrafic <troczynska.anna@gmail.com>
2021-03-28 16:10:11 +0200

    json update

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+fetch("https://github.com/atgrafic/projekt/blob/master/json/list.json", { mode: "no-cors" })
+  .then(response => response.json())
+  .then(data => console.log(data));
 
-fetch("https://github.com/atgrafic/cw/blob/master/jenzyk.txt", {mode: 'no-cors'})
-.then(response => response.json())
-.then(json => console.log(json));
+// const carsList_url = "https://github.com/atgrafic/projekt/blob/master/json/list.json";
 
+//     async function carsList() {
+//       const response = await fetch('carsList_url',{ mode: "no-cors" });
+//       const json = await response.json();
+//       const { message: imageUrl } = await result.json();
+
+//       carsList
+//     }
+
+//     carsList();

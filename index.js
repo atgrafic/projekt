dbf2e3d3a2b5cb0a62b8f36ea181757e821d2ebe
atgrafic <troczynska.anna@gmail.com>
2021-03-28 16:36:52 +0200

    json update 2

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-fetch("https://github.com/atgrafic/projekt/blob/master/json/list.json", { mode: "no-cors" })
-  .then(response => response.json())
-  .then(data => console.log(data));
+// fetch("https://github.com/atgrafic/projekt/blob/master/json/list.json", { mode: "no-cors" })
+//   .then(response => response.json())
+//   .then(data => console.log(data));
 
-// const carsList_url = "https://github.com/atgrafic/projekt/blob/master/json/list.json";
+const carsList_url = "./json/list.json";
 
-//     async function carsList() {
-//       const response = await fetch('carsList_url',{ mode: "no-cors" });
-//       const json = await response.json();
-//       const { message: imageUrl } = await result.json();
+async function carsList() {
+    const response = await fetch(carsList_url, { mode: "no-cors" });
+    console.log(response);
+    const json = await response.json();
+    console.log(json);
+    document.getElementById("carsList").innerHTML = json;
 
-//       carsList
-//     }
+}
 
-//     carsList();
+carsList();
